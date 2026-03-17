fa69ab1fdfbfb959582ca7ff8e5fa2eb5186b750
Eknoor Singh <eknoor.singh@student.kuleuven.be>
2026-03-17 14:54:17 +0100

    Updated Transformer variables

---
+++ windfarm_params.m
@@ -0,0 +1,12 @@
+% This file has the purpose of declaring all the variables that are used 
+% in the blocks 
+
+%%Substation Cluster B
+
+P_b_nom = 600e6;
+P_a3_8_1_nom = 450e6;
+P_a3_8_2_nom = 450e6;
+P_a3_7_nom = 450e6;
+
+Vac = 380e3;
+Vnom = 66e3;
