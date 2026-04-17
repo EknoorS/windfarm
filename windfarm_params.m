292477dd691db9b0caa5a416cf3b3ebc6dba67a6
fitch <thomas.fitch@hotmail.com>
2026-04-17 14:04:48 +0200

    Added onshore substation

--- windfarm_params.m
+++ windfarm_params.m
@@ -3,12 +3,14 @@
 
 %%Substation Cluster B
 
+P_onshore = 500e6;
 P_b_nom = 600e6;
 P_a3_8_1_nom = 450e6;
 P_a3_8_2_nom = 450e6;
 P_a3_7_nom = 450e6;
 
-Vac = 380e3;
+Vgrid = 380e3;
+Vac = 220e3;
 Vnom = 66e3;
 
 
